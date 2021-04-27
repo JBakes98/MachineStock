042102d079bd8143671e4e610f08eb2af9b73dd1
joshb <joshbaker286.jb@gmail.com>
2021-04-27 13:02:25 +0100

    Added apps to installed apps

--- final_year_artefact/base_settings.py
+++ final_year_artefact/base_settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'final_year_artefact',
+    'stock_prediction',
 ]
 
 MIDDLEWARE = [
