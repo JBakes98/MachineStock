0e05ba369de59f7b828552491cc74a9390aa381a
joshb <joshbaker286.jb@gmail.com>
2021-04-27 15:23:30 +0100

    Commented out logic that isnt implemented fully yet as it makes it hard to test

--- final_year_artefact/views.py
+++ final_year_artefact/views.py
@@ -12,16 +12,16 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
 
-        prev_weekday = date_utils.prev_weekday(datetime.date.today())
-        context['data_date'] = StockData.objects.all().order_by('-date')[:1]
-        context['latest_weekday'] = prev_weekday
-
-        context['top_winners'] = StockData.objects.filter(
-            date=[prev_weekday]
-        ).order_by('-date', '-change_perc')[:5]
-
-        context['top_losers'] = StockData.objects.filter(
-            date=prev_weekday
-        ).order_by('-date', 'change_perc')[:5]
+        # prev_weekday = date_utils.prev_weekday(datetime.date.today())
+        # context['data_date'] = StockData.objects.all().order_by('-date')[:1]
+        # context['latest_weekday'] = prev_weekday
+        #
+        # context['top_winners'] = StockData.objects.filter(
+        #     date=[prev_weekday]
+        # ).order_by('-date', '-change_perc')[:5]
+        #
+        # context['top_losers'] = StockData.objects.filter(
+        #     date=prev_weekday
+        # ).order_by('-date', 'change_perc')[:5]
 
         return context
