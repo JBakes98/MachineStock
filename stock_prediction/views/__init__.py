f985d9054adcf1bbd85134e0f6f2f1502fffd2da
joshb <joshbaker286.jb@gmail.com>
2021-04-27 23:56:23 +0100

    Updated url paths to use new views

--- stock_prediction/views/__init__.py
+++ stock_prediction/views/__init__.py
@@ -1,5 +1,5 @@
 from .index import IndexView
 from .stock_list import StockList
 from .stock_detail import StockDetail
-from .add_stocks_background_endpoint import add_stocks_background_endpoint
-from .collect_stock_data_endpoint import collect_stock_data_endpoint
+from .add_stocks_background_endpoint import AddStocksEndpoint
+from .collect_stock_data_endpoint import CollectStockDataEndpoint
