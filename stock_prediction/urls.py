80b06218bfb5bfa55815e594f68bb35f1ebb75f8
joshb <joshbaker286.jb@gmail.com>
2021-04-27 23:26:32 +0100

    Removed brackets from paths

--- stock_prediction/urls.py
+++ stock_prediction/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
     path('stocks/', views.StockList.as_view(), name='stock-list'),
     path('stocks/<str:ticker>', views.StockDetail.as_view(), name='stock-detail'),
-    path('add-stocks-background-endpoint/', views.add_stocks_background_endpoint(), name='add-stocks-background-endpoint'),
-    path('collect-stock-data-endpoint/', views.collect_stock_data_endpoint(), name='collect-stock-data-endpoint'),
+    path('add-stocks-endpoint/', views.add_stocks_background_endpoint, name='add-stocks-background-endpoint'),
+    path('collect-stock-data-endpoint/', views.collect_stock_data_endpoint, name='collect-stock-data-endpoint'),
 ]
