7e4b3792cb129ae88edcd2161ff375dbb1c20fb7
Josh Baker <josh.baker286.jb@gmail.com>
2021-04-29 13:01:40 +0100

    Correcting template errors

--- stockprediction/views/stock_detail.py
+++ stockprediction/views/stock_detail.py
@@ -23,7 +23,7 @@ class StockDetail(DetailView):
         context = super(StockDetail, self).get_context_data(**kwargs)
         latest_weekday = date_utils.prev_weekday(datetime.date.today())
         stock_data = self.object.latest_data()
-        context['stock_date'] = stock_data
+        context['stock_data'] = stock_data
         context['refresh'] = stock_data.date.date() == latest_weekday
         context['ti_chart'] = self.object.plot_technical_indicators()
 
