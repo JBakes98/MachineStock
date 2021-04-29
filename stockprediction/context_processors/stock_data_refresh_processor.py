de4dc8b5812880aa378c66c024cc02467a0006d4
Josh Baker <josh.baker286.jb@gmail.com>
2021-04-29 12:45:36 +0100

    Select first StockData model

--- stockprediction/context_processors/stock_data_refresh_processor.py
+++ stockprediction/context_processors/stock_data_refresh_processor.py
@@ -5,7 +5,7 @@ from stockprediction.utils import date_utils
 
 
 def stock_data_refresh_processor(request):
-    data_date = StockData.objects.all().order_by('-date')[:1]
+    data_date = StockData.objects.all().order_by('-date').first()  # Select first latest data
     data_date = data_date.date  # Assign to the date attribute of the model
     latest_weekday = date_utils.prev_weekday(datetime.date.today())  # Get the previous market day
     return {'data_refresh': data_date.date() == latest_weekday}
