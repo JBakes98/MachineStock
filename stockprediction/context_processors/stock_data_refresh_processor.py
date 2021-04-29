15012f21d78d98264dfbe63a801767def32b286a
Josh Baker <josh.baker286.jb@gmail.com>
2021-04-29 12:41:28 +0100

    Corrected data refresh context processor

--- stockprediction/context_processors/stock_data_refresh_processor.py
+++ stockprediction/context_processors/stock_data_refresh_processor.py
@@ -6,5 +6,6 @@ from stockprediction.utils import date_utils
 
 def stock_data_refresh_processor(request):
     data_date = StockData.objects.all().order_by('-date')[:1]
-    latest_weekday = date_utils.prev_weekday(datetime.date.today())
-    return {'data_refresh': data_date.date == latest_weekday}
+    data_date = data_date.date  # Assign to the date attribute of the model
+    latest_weekday = date_utils.prev_weekday(datetime.date.today())  # Get the previous market day
+    return {'data_refresh': data_date.date() == latest_weekday}
