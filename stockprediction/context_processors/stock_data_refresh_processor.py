16c829629e08c53b6e55916ee9e4a8d38cb73867
joshb <joshbaker286.jb@gmail.com>
2021-05-07 22:48:32 +0100

    Updated neutral icon and trying to correct if data is out of date

--- stockprediction/context_processors/stock_data_refresh_processor.py
+++ stockprediction/context_processors/stock_data_refresh_processor.py
@@ -8,4 +8,4 @@ def stock_data_refresh_processor(request):
     data_date = StockData.objects.all().order_by('-date').first()  # Select first latest data
     data_date = data_date.date  # Assign to the date attribute of the model
     latest_weekday = date_utils.prev_weekday(datetime.date.today())  # Get the previous market day
-    return {'data_refresh': data_date.date() == latest_weekday}
+    return {'data_refresh': data_date.date() != latest_weekday}
